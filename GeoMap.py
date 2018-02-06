c1b917974b4b9c78f94acd4a49d8bdc1d97640ef
CREUTZBURG Brian <bcreutz@deq.state.or.us>
2018-02-06 08:44:36 -0800

    updates that were suggested

--- GeoMap.py
+++ GeoMap.py
@@ -1,13 +1,20 @@
 import arcpy
 
-mxd = arcpy.mapping.MapDocument(r"\\DEQHQ1\TMDL\TMDL_WR\MidCoast\GIS\Figures\Upper_Yaquina_Maps\Upper_Yaquina_TMDL_Blank_ME.mxd")
-df = arcpy.mapping.ListDataFrames(mxd, "Layers")[0]
+str_dir_main = r"\\DEQHQ1\TMDL\TMDL_WR\MidCoast\GIS\Figures\Upper_Yaquina_Maps"
+str_mxd_blank = r"Upper_Yaquina_TMDL_Blank_ME.mxd"
+str_df_cur = r"Layers"
+str_lyr_cur = r"UY_Geo.lyr"
+str_mxd_save = r"Upper_Yaquina_TMDL_UY_Geo.mxd"
 
-addLayer2 = arcpy.mapping.Layer(r"\\DEQHQ1\TMDL\TMDL_WR\MidCoast\GIS\Figures\Upper_Yaquina_Maps\UY_Geo.lyr")
+mxd = arcpy.mapping.MapDocument(str_dir_main + "\\" + str_mxd_blank)
+df = arcpy.mapping.ListDataFrames(mxd, str_df_cur)[0]
+
+addLayer2 = arcpy.mapping.Layer(str_dir_main + "\\" + str_lyr_cur)
 
 arcpy.mapping.AddLayer(df, addLayer2, "BOTTOM")
+arcpy.RefreshTOC()
+arcpy.RefreshActiveView()
 
-mxd.saveACopy(r"\\DEQHQ1\TMDL\TMDL_WR\MidCoast\GIS\Figures\Upper_Yaquina_Maps\Upper_Yaquina_TMDL_UY_Geo.mxd")
+mxd.saveACopy(str_dir_main + "\\" + str_mxd_save)
 
-del mxd, addLayer2
-
+del mxd, df, addLayer2
